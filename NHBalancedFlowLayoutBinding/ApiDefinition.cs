54e6337453bf394435e444635c14e87a20186e8b
Aleksandr Moshkin <moshkin_alexander@outlook.com>
2017-10-11 19:12:11 +0300

    Initial commit

---
+++ NHBalancedFlowLayoutBinding/ApiDefinition.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+
+namespace NHBalancedFlowLayoutBinding
+{
+    [BaseType(typeof(UICollectionViewDelegateFlowLayout))]
+    [Model]
+    [Protocol]
+    interface NHBalancedFlowLayoutDelegate
+    {
+        [Export("collectionView:layout:preferredSizeForItemAtIndexPath:")]
+        [Abstract]
+        CGSize PreferredSizeForItemAtIndexPath(UICollectionView cv, NHBalancedFlowLayout layout, NSIndexPath indexPath);
+    }
+
+    [BaseType(typeof(UICollectionViewFlowLayout))]
+    interface NHBalancedFlowLayout
+    {
+    }
+}
